a0126f854f9f99a48360b223b3eeafb83dc134ed
hannahoeidahl <hanhoi02118@stud.noroff.no>
2025-10-12 14:14:22 +0200

    feat: configure Tailwind colors and typography

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,21 @@
 export default {
   darkMode: "class",
   content: ["./index.html", "./src/**/*.{js,jsx,ts,tsx}"],
-  theme: { extend: {} },
+  theme: {
+    extend: {
+      colors: {
+        bg: { light: "#EFEFEF", dark: "#3C3C3C" },
+        text: { light: "#3C3C3C", dark: "#F8F8F8" },
+        butter: { DEFAULT: "#F7F6D4", hover: "#EDECBF" },
+        lavender: { DEFAULT: "#AFA8C8", hover: "#9F97BC" },
+        teal: { DEFAULT: "#306572", hover: "#27545F" },
+        elev: "#F8F8F8",
+      },
+      fontFamily: {
+        display: ["Josefin Slab", "serif"],
+        body: ["Josefin Sans", "system-ui", "sans-serif"],
+      },
+    },
+  },
   plugins: [],
 };
-
